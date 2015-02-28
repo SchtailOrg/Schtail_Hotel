7ce8b16cbca9290762db8df202f9f80aa291e2ea
Kilian <kilian@schtail.com>
2015-02-28 12:16:25 +0100

    ajax loading prototype
    onyl the site content is loaded

---
+++ head.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Hotel Schtail *****</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="site.css" media="screen">
+  <script type="text/javascript" src="jquery.js"></script>
+</head>
+<body>
+<?php  include 'nav.php'; ?>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php include 'head.php'; ?>
+<main><?php include 'sites/test.php'; ?></main>
+<?php  include 'footer.php'; ?>
